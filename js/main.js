d4b71991095d11e9e4df75309148678d82bd7a62
Federico Commisso <fdcommisso@gmail.com>
2024-10-15 15:12:06 -0400

    add gtag

--- js/main.js
+++ js/main.js
@@ -11,4 +11,10 @@ window.onload = function () {
   //   .setPin('#my-sticky-element') // pins the element for the the scene's duration
   //   .addTo(controller); // assign the scene to the controller
 
+  // set .echo-date span content to current year
+  const dateSpan = document.querySelector('.echo-date');
+  if (dateSpan) {
+    dateSpan.textContent = new Date().getFullYear();
+  }
+
 }
